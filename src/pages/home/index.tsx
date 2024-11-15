1ae9e984d48b4bfa25816305a0c5e83d9f3af363
Romeu Lima <romeu.sousadp@gmail.com>
2024-11-15 13:18:06 -0300

    change the validation error name

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,7 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import zod from 'zod'
 
 const newCycleFormValidationSchema = zod.object({
-    task: zod.string().min(1, 'Informe a tarefa'),
+    task: zod.string().min(1, 'Input the task!'),
     minutesAmount: zod.number().min(5).max(60)
 })
 
